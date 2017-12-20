0b77857b4f03c7274ac821977673227c89e63657
hwd <huwd@adnonstop.com>
2017-12-20 14:15:54 +0800

    Initial Commit

---
+++ activityPageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  activityPageDemo
+//
+//  Created by 胡伟东 on 20/12/17.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ activityPageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  activityPageDemo
+//
+//  Created by 胡伟东 on 20/12/17.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
